ff349cdacd1f03d9c584aab5a2030072ba20ae42
Hinoshii <110216402+Hinoshii@users.noreply.github.com>
2022-09-21 22:31:08 +0700

    Update week4-2.c

--- week4-2.c
+++ week4-2.c
@@ -1,43 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int Arr[10];
+    int a,b,c,d,e,f,g,h,i,j;
     printf("Enter your Array member(10) : ");
-    for(int i=0;i<10;i++)
-    {
-        scanf("%d",&Arr[i]);
-    }
+    scanf("%d %d %d %d %d %d %d %d %d %d",&a,&b,&c,&d,&e,&f,&g,&h,&i,&j);
 
-    int even[5];
-    int odd[5];
-    int o=0;
-    int e=0;
-    int od=0;
-    int ev=0;
-
-    for(int i=0;i<10;i++)
-    {
-        if(i%2==1)
-        {
-            even[e]=Arr[i];
-            e++;
-        }
-
-        else if(i%2==0)
-        {
-            odd[o]=Arr[i];
-            o++;
-        }
-    }
-
-    for(int i=0;i<5;i++)
-    {
-        ev+=even[i];
-    }
-    for(int i=0;i<5;i++)
-    {
-        od+=odd[i];
-    }
+    int od = a+c+e+g+i;
+    int ev = b+d+f+h+j;
 
     printf("Result = %d",od-ev);
 }
